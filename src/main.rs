15df996e3f289f38787cddd1069bff3c3ea8e4e9
Patrick Miller <paddymills@pm.me>
2022-05-19 13:19:01 -0400

    add htmx

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 #[macro_use] extern crate rocket;
 
+use rocket_dyn_templates::Template;
+use rocket::serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Ctx<'r> {
+    val: &'r str
+}
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", Ctx { val: "test" })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
