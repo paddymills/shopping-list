5515601af93cf1c9820d8d733a085188710ca15e
Patrick Miller <paddymills@pm.me>
2022-05-21 13:06:48 -0400

    add static

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use] extern crate rocket;
 
 use rocket_dyn_templates::Template;
-use rocket::serde::{Serialize, Deserialize};
+use rocket::{
+    serde::{Serialize, Deserialize},
+    fs::FileServer
+};
 
 #[derive(Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -18,5 +21,6 @@ fn index() -> Template {
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .mount("/public", FileServer::from("static/"))
         .attach(Template::fairing())
 }
